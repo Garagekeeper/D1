f1e2d8dbeaa68b8bcae7edf484c7362450430559
주형진 <hj06869@naver.com>
2026-05-19 10:44:10 +0900

    초기 커밋.

---
+++ D1/D1.cpp
@@ -0,0 +1,42 @@
+#include "D1.h"
+
+void Init()
+{
+
+}
+
+void Input()
+{
+    
+}
+
+void Update()
+{
+
+}
+
+void Render()
+{
+    system("cls");
+
+}
+
+void GotoXY()
+{
+    
+}
+
+int main()
+{
+    Init();
+    while (true)
+    {
+        Input();
+        Update();
+        Render();
+    }
+
+    printf("Hello World");
+}
+
+
---
+++ D1/D1.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+typedef struct Player
+{
+	int x;
+	int y;
+};
+
+void GotoXY();
+
